569769b51db0dddd9f738a0e30c4ed138cc02041
WasteLand <karimdzhumakov21082007@gmail.com>
2022-04-27 21:59:20 +0300

    Publish

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+    $value1 = "Main";
+    $value2 = "Windows/Other";
+    setcookie("Page", $value1);
+    setcookie("User", $value2, time()+3600);  // срок действия 1 час
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Preview -->
+    <meta property="og:title" content="WasteLand Official site">
+    <meta property="og:site_name" content="WasteLand">
+    <meta property="og:url" content="https://www.wlorigin.cf/">
+    <meta property="og:description" content="WasteLand Origin community">
+    <meta property="og:image" content="https://www.wlorigin.cf/images/logo2.webp">
+    <!-- Fonts and css -->
+    <link rel="stylesheet" href="assets/css/main.css?v=24">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Montserrat:400,700&amp;subset=cyrillic-ext" rel="stylesheet">
+    <link href="https://allfont.ru/allfont.css?fonts=furore" rel="stylesheet" type="text/css" />
+    <!-- Title -->
+    <title>WasteLand</title>
+</head>
+<body>
+
+<!-- Library -->
+<script src="assets/js/particles.min.js"></script>
+
+<!-- Code -->
+
+<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <div class="header__logo"><div class="text_orange_big">WasteLand [Beta Site]</div></div>
+
+            <nav class="nav">
+                <a class="nav__link active" href="#">About</a>
+                <a class="nav__link" href="buy.html">Buy Pass</a>
+                <a class="nav__link" href="buy_premium.html">Premium</a>
+                <a class="nav__link" href="documents.html">Docs</a>
+                <a class="nav__link" href="https://discord.com/invite/UBaauaN">Discord</a>
+                <a class="nav__link" href="contact.html">Contact</a>
+            </nav>
+        </div>
+    </div>
+</header>
+
+
+<div class="intro">
+    <div class="container">
+        <div class="intro__inner">
+            <h2 class="intro__suptitle">WasteLand</h2>
+            <h1 class="intro__title">Best Vanilla - Political RP Server</h1>
+
+            <a class="btn" href="server.html">Server perfomance</a>
+        </div>
+    </div>
+
+    <div class="slider">
+        <div class="container">
+            <div class="slider__inner">
+                <div class="slider__item active">
+                    <span class="slider__num">01</span>
+                    Welcome
+                </div>
+                <div class="slider__item">
+                    <span class="slider__num">02</span>
+                    To
+                </div>
+                <div class="slider__item">
+                    <span class="slider__num">03</span>
+                    The
+                </div>
+                <div class="slider__item">
+                    <span class="slider__num">04</span>
+                    WasteLand
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="intro2">
+    <div class="intro3">
+        <div class="container">
+            <div class="intro__inner">
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>
+    <h1 class="text_orange">WasteLand</h1>
+    <h1 class="text_orange">Best Vanilla - Political Server</h1>
+</p>
+<p>
+    <h1 class="text_white">Convenient world</h1>
+    <h1 class="text_white">We present you our best server project, we hope you enjoy it.</h1>
+</p>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
